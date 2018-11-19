761177f3546a91d1cad10c91af42208e934e6d26
tapos <tapos.aa@gmail.com>
2018-11-20 02:15:09 +0600

    onchange event in search page work done

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,20 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class SearchBar extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {searchValue: ''};
+        // this.onInputChange = this.onInputChange.bind(this);
+    }
+
+    // onInputChange(event) {
+    //     this.setState({searchValue: event.target.value});
+    // }
+
     render() {
         return (
-            <input className="form-control form-control-lg" type="text" placeholder="search"/>
+            <input className="form-control form-control-lg" value={this.state.searchValue} onChange={event =>this.setState({searchValue: event.target.value}) } type="text" placeholder="search"/>
         );
     }
 }
